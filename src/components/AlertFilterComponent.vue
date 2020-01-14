a32c44f94b950b418a109fbe41a0c2b10a7dbc8b
babs12316 <44439609+babs12316@users.noreply.github.com>
2020-01-14 23:24:26 +0100

    Alert Filter changes

---
+++ src/components/AlertFilterComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ alerttype }}
+ <!--   <button v-on:click="$emit('enlarge-text', 0.1)">
+      Enlarge text
+    </button>-->
+    <select v-model="alerttype" v-on:change="$emit('alerttype', alerttype)">
+      <option disabled value="">Please select one</option>
+      <option value="yellow">Yellow</option>
+      <option value="green">Green</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      alerttype: null,
+      info: null
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,41 @@
 <template>
   <div class="container-fluid">
-    <hr />
-    <div v-for="section in info" :key="section.index">
-      <div
-        class="card col-xs-12 col-sm-12 col-md-6 col-lg-6"
-        v-for="earthquake in section.features"
-        :key="earthquake.mag"
-      >
-        <!--location of the epicenter lang & lat -->
-        <MapComponent
-          :ylng="earthquake.geometry.coordinates[0]"
-          :xlat="earthquake.geometry.coordinates[1]"
-        ></MapComponent>
-        <br />
-        <!-- Place -->
-        <PlaceComponent :place="earthquake.properties.place"></PlaceComponent>
-        <!-- Time -->
-        <TimeComponent :time="earthquake.properties.time"></TimeComponent>
-        <!-- Magnititude -->
-        <MagnitudeComponent
-          :mag="earthquake.properties.mag"
-        ></MagnitudeComponent>
-        <!-- Alert -->
-        <AlertComponent :alert="earthquake.properties.alert"></AlertComponent>
-        <!-- tsunami -->
-        <TsunamiComponent
-          :tsunami="earthquake.properties.tsunami"
-        ></TsunamiComponent>
-        <!-- earthquake occurrence page -->
-        <OccuranceUrlComponent
-          :url="earthquake.properties.url"
-        ></OccuranceUrlComponent>
-        <hr />
+    <div class="filters">
+      <div class="col-sm-2">
+        <AlertFilterComponent
+          v-on:alerttype="updateAlertType($event)"
+        ></AlertFilterComponent>
       </div>
+    </div>
+    <hr />
+    <div
+      v-for="earthquake in details"
+      :key="earthquake.index"
+      class="card col-xs-12 col-sm-12 col-md-6 col-lg-6"
+    >
+      <!--location of the epicenter lang & lat -->
+      <MapComponent
+        :ylng="earthquake.geometry.coordinates[0]"
+        :xlat="earthquake.geometry.coordinates[1]"
+      ></MapComponent>
+      <br />
+      <!-- Place -->
+      <PlaceComponent :place="earthquake.properties.place"></PlaceComponent>
+      <!-- Time -->
+      <TimeComponent :time="earthquake.properties.time"></TimeComponent>
+      <!-- Magnititude -->
+      <MagnitudeComponent :mag="earthquake.properties.mag"></MagnitudeComponent>
+      <!-- Alert -->
+      <AlertComponent :alert="earthquake.properties.alert"></AlertComponent>
+      <!-- tsunami -->
+      <TsunamiComponent
+        :tsunami="earthquake.properties.tsunami"
+      ></TsunamiComponent>
+      <!-- earthquake occurrence page -->
+      <OccuranceUrlComponent
+        :url="earthquake.properties.url"
+      ></OccuranceUrlComponent>
+      <hr />
     </div>
   </div>
 </template>
@@ -47,13 +50,16 @@ import AlertComponent from "@/components/AlertComponent.vue";
 import TsunamiComponent from "@/components/TsunamiComponent.vue";
 import OccuranceUrlComponent from "@/components/OccuranceUrlComponent.vue";
 import MapComponent from "@/components/MapComponent.vue";
+import AlertFilterComponent from "@/components/AlertFilterComponent.vue";
 
 const axios = require("axios");
 export default {
   name: "home",
   data: function() {
     return {
-      info: null
+      info: null,
+      details: null,
+      alertfilter: new RegExp(".")
     };
   },
   mounted() {
@@ -61,7 +67,19 @@ export default {
       .get(
         "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
       )
-      .then(response => (this.info = response));
+      .then(
+        response =>
+          (this.info = response.data.features) && //Store json data in 2 variables
+          (this.details = response.data.features)
+      );
+  },
+  methods: {
+    updateAlertType: function(alertvalue) {
+      this.details = this.info; //get original Json
+      this.details = this.details.filter(
+        detail => detail.properties.alert == alertvalue
+      );
+    }
   },
   components: {
     PlaceComponent,
@@ -70,7 +88,8 @@ export default {
     AlertComponent,
     TsunamiComponent,
     OccuranceUrlComponent,
-    MapComponent
+    MapComponent,
+    AlertFilterComponent
   }
 };
 </script>
